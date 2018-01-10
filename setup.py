54053dceeb0ca7cfaa06ea50485d017f57af77d7
cagdas <cagdas.oztekin@mobilearts.com>
2018-01-10 14:40:47 +0100

    Add comma

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='Scrampy',
      author='Cagdas',
      author_email="cagdas@a.b",
      url='https://www.pundun.io',
-     license='Apache License 2.0'
+     license='Apache License 2.0',
      packages=find_packages(exclude=["tests*"])
     )
